4e8e2eec4b41339d6cc3180069a8f1f6b00a56b3
nhann <nhanhnv@gmail.com>
2024-03-27 18:12:04 +0700

    update all  code

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { ConfigModule } from '@nestjs/config';
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-            envFilePath: `.env.${process.env.NODE_ENV}.local`,
+            envFilePath: '.env',
         }),
         DeviceModule,
         DatabaseModule,
